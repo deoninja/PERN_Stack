55d8fb61b2de71a4cd39a8bbc2a8986666bf3d96
deo <deoninja@gmail.com>
2025-02-13 15:02:07 +0800

    CRUD products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {getAllProducts, createProduct} from "../controllers/products.js"
+import {getAllProducts, createProduct, getProduct, updateProduct, deleteProduct} from "../controllers/products.js"
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
-
+router.get("/:id", getProduct);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 
 export default router;
