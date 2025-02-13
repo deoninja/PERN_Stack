53ad97028cd126784d948c398c8930fc040db3ca
deo <deoninja@gmail.com>
2025-02-13 14:23:34 +0800

    database setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import productRoutes from "./routes/productRoutes.js"
+import {sql} from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+
 
 dotenv.config();
 
@@ -15,8 +17,26 @@ app.use(cors());
 app.use(helmet()); // for security
 app.use(morgan("dev")); //Log the request
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
+async function initDB() {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY, 
+            name VARCHAR(255) NOT NULL, 
+            image VARCHAR(255) NOT NULL, 
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+        console.log("Database initialized successfully")
+    } catch (error) {
+        console.log("Error initDB", error)
+    }
+}
+
+initDB().then(() =>{
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`)
+    })
 })
