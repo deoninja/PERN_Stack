c3a806a3096e5cc191a66a3cbbabc765a75f7759
deo <deoninja@gmail.com>
2025-02-15 15:36:13 +0800

    frontend setuo

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
